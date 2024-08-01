92f8bdf8c8182bea78e7b271994aa2a611325a62
gicellyn <gicellynayara@gmail.com>
2024-08-01 15:15:39 -0300

    atualizações

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import { authenticate, connection } from "./config/database.js";
-import { Aluno } from "./models/aluno.js";
-import { Endereco } from "./models/endereco.js";
-import { Treino } from "./models/treino.js";
 import express from "express";
+import { alunosRouter } from "./routes/alunos.js";
+import { treinosRouter } from "./routes/treinos.js";
 
 authenticate(connection).then(() => {
     connection.sync();
@@ -12,81 +11,8 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Bem-vindo a CodeFitness!")
-})
-
-app.get("/alunos", async (req, res) => {
-    const listaAlunos = await Aluno.findAll();
-    res.send(listaAlunos);
-});
-
-app.get("/alunos/:id", async (req, res) => {
-   
-    const aluno = await Aluno.findOne({
-        where: { id: req.params.id },
-        include: [Endereco]
-    })
-    if (aluno) {
-        res.json(aluno);
-    } else {
-        res.status(404).json({ message: "Aluno não encontrado" });
-    }
-    
-})
-
-app.post("/alunos", async (req, res) => {
-    const { nome, email, telefone, endereco } = req.body;
-
-    try {
-        //Tentativa de inserir o cliente
-        await Aluno.create(
-            { nome, email, telefone, endereco },
-            { include: [Endereco] },
-        );
-        res.json({ message: "Aluno criado com sucesso" });
-    } catch (err) {
-        res.status(500).json({ message: "Um erro ocorreu ao inserir aluno." })
-    }
-});
-
-app.put("/alunos/:id", async (req, res) => {
-    const idAluno = req.params.id;
-    const { nome, email, telefone, endereco } = req.body
-
-    try {
-        const aluno = await Aluno.findOne({ where: { id: idAluno } });
-
-        if (aluno) {   
-            await Endereco.update(endereco, { where: { AlunoId: idAluno } })
-            await aluno.update({ nome, email, telefone });
-            res.json({ message: "Aluno atualizado." });
-        } else {
-            res.status(404).json({ message: "Aluno não encontrado." });
-        }
-    } catch (error) {
-        res.status(500).json({ message: "Ocorreu um erro ao atualizar o aluno." });
-    }
-
-});
-
-app.delete("/alunos/:id", async (req, res) => {
-    const idAluno = req.params.id;
-    
-    try {
-        const aluno = await Aluno.findOne({ where: { id: idAluno } }); 
-
-        if (aluno) {
-            await aluno.destroy();
-            res.json({message: "Aluno removido com sucesso."})
-
-        } else {
-            res.status(404).json({ message: "Aluno não encontrado." });
-        }
-    } catch (error) {
-        res.status(500).json({ message: "Ocorreu um erro ao excluir o aluno." });
-    }
-})
+app.use(alunosRouter);
+app.use(treinosRouter);
 
 
 app.listen(3000, () => {
