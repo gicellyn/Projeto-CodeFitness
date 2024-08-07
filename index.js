f349d7b5583f9bfb55fe7eecdf09b48b15059549
gicellyn <gicellynayara@gmail.com>
2024-08-07 16:35:59 -0300

    adição da dependencia axios

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { authenticate, connection } from "./config/database.js";
 import express from "express";
 import { alunosRouter } from "./routes/alunos.js";
 import { treinosRouter } from "./routes/treinos.js";
+import cors from "cors";
 
 authenticate(connection).then(() => {
     connection.sync();
@@ -10,6 +11,8 @@ authenticate(connection).then(() => {
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({origin: "http://localhost:5173"}));
 
 app.use(alunosRouter);
 app.use(treinosRouter);
